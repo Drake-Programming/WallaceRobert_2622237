c537869ff6662c7777da5c1ce4baf7378d30e570
Robert Wallace <Robert Wallace>
2016-12-10 14:26:32 -0800

    Signed-off-by: Robert Wallace <Robert Wallace>

---
+++ hmwk/Template/main.cpp
@@ -0,0 +1,25 @@
+/* 
+   File:   main
+   Author: Robert D. Wallace
+   Created on October 21st, 11:30 PM
+   Purpose:  MidTerm
+ */
+
+//System Libraries
+#include <iostream>   //Input/Output objects
+using namespace std;  //Name-space used in the System Library
+
+//User Libraries
+
+//Global Constants
+
+//Function prototypes
+
+//Execution Begins Here!
+
+int main(int argc, char** argv) {
+    //Declaration of Variables
+
+    //Exit Program
+    return 0;
+}
